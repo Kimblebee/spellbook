dfa5d41d51c2faf7466972db19ec3cb20eeb3f56
Kimberly Crevecoeur <kimberlycrev@gmail.com>
2018-06-05 09:08:08 -0400

    changed fonts, colors, and added a marquee

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// Kimberly Rose Crevecoeur
+
 const reveal = document.querySelector('#reveal')
 const revealMimic = function () {
     const heading = document.querySelector('#mimic')
-    heading.textContent = "I am the mimic!"
+    heading.textContent = "The mimic has revealed itself!"
 }
-
+// adding the eventlistener to the "reveal the mimic!" button
 reveal.addEventListener('click', revealMimic)
 
 
+
+// function to change the second header based on the input
 const submit = function () {
-    const heroHeading = document.querySelector('#mimic')
+    const heroHeading = document.querySelector('#mimic') // heroname and heroclass are the inputs
     const heroName = document.querySelector('#name').value
     const heroClass = document.querySelector('#class').value
 
