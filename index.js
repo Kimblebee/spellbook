c2863ecd5a064494bc8306a945e8d188ec44e528
Kimberly Crevecoeur <kimberlycrev@gmail.com>
2018-06-04 21:07:47 -0400

    added form functionality

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('testing')
+const reveal = document.querySelector('#reveal')
+const revealMimic = function () {
+    const heading = document.querySelector('#mimic')
+    heading.textContent = "I am the mimic!"
+}
+
+reveal.addEventListener('click', revealMimic)
+
+
+const submit = function () {
+    const heroHeading = document.querySelector('#mimic')
+    const heroName = document.querySelector('#name').value
+    const heroClass = document.querySelector('#class').value
+
+    const heading = document.querySelector('#mimic')
+
+    heroHeading.textContent = heroName + " the " + heroClass + " has slain the Treasure chest mimic!"
+
+}
+
+
+
+const submitButton = document.querySelector("#character")
+
+submitButton.addEventListener('click', submit)
